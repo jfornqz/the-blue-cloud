c1c7b752ea68c574629fe0922dc5a25e847b233f
pspapleng <chayanin3343@gmail.com>
2022-05-27 08:07:48 +0700

    fix: handle bug and change gql uri

--- frontend/src/graphql/query.js
+++ frontend/src/graphql/query.js
@@ -11,7 +11,7 @@ export const QUERY_ME = gql`
     }
 `
 
-export const ALL_POSTS = gql`
+export const ALL_POST = gql`
     {
         posts {
             _id
@@ -43,7 +43,7 @@ export const QUERY_POST = gql`
     }
 `
 
-export const ALL_FORMS = gql`
+export const ALL_FORM = gql`
     {
         forms {
             _id
--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -16,7 +16,7 @@ const Router = ({ user, cookie }) => {
         <Fragment>
             <Routes>
                 {!user && !cookie.token && (
-                    <Route path="/login" element={<ViewFormPage />} />
+                    <Route path="/login" element={<LoginPage />} />
                 )}
 
                 {user?.email === 'admin' ? (
