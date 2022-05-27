9d3030089d715c2c2025cda40ac08791125ba995
pspapleng <chayanin3343@gmail.com>
2022-05-27 09:07:34 +0700

    why don't work

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -6,7 +6,7 @@ import LoginPage from '../pages/loginpage/Loginpage'
 import Dashboard from '../pages/admin/dashboard/Dashboard'
 import HomePage from '../pages/student/homepage/Home'
 import ViewFormPage from '../pages/student/viewform/Viewform'
-import DescPage from '../pages/student/descpage/desc'
+import DescPage from '../pages/student/descpage/Desc'
 import ProfilePage from '../pages/student/profilepage/Profile'
 import Formspage from '../pages/admin/formspage/Formspage'
 import Postspage from '../pages/admin/postspage/Postspage'
@@ -17,7 +17,6 @@ import NewForm from '../pages/admin/formspage/NewForm'
 import EditForm from '../pages/admin/formspage/EditForm'
 
 const Router = ({ user, cookie }) => {
-
     return (
         <Fragment>
             <Routes>
@@ -25,30 +24,32 @@ const Router = ({ user, cookie }) => {
                     <Route path="/login" element={<LoginPage />} />
                 )}
 
-                {
-                    user?.role === 'Admin' && (
-                        <>
-                            <Route path="/dashboard" element={<Dashboard />} />
-                            <Route path="/posts" element={<Postspage />} />
-                            <Route path="/forms" element={<Formspage />} />
-                            <Route path="/submission/:formId" element={<Submissionpage />} />
-                            <Route path="/newPost" element={<NewPost />} />
-                            <Route path="/editPost" element={<EditPost />} />
-                            <Route path="/newForm" element={<NewForm />} />
-                            <Route path="/editForm" element={<EditForm />} />
-                        </>
-                    )
-                }
-                {
-                    user?.role === 'Student' && (
-                        <>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/view" element={<ViewFormPage />} />
-                            <Route path="/profile" element={<ProfilePage />} />
-                            <Route path="/description/:title" element={<DescPage />} />
-                        </>
-                    )
-                }
+                {user?.role === 'Admin' && (
+                    <>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/posts" element={<Postspage />} />
+                        <Route path="/forms" element={<Formspage />} />
+                        <Route
+                            path="/submission/:formId"
+                            element={<Submissionpage />}
+                        />
+                        <Route path="/newPost" element={<NewPost />} />
+                        <Route path="/editPost" element={<EditPost />} />
+                        <Route path="/newForm" element={<NewForm />} />
+                        <Route path="/editForm" element={<EditForm />} />
+                    </>
+                )}
+                {user?.role === 'Student' && (
+                    <>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/view" element={<ViewFormPage />} />
+                        <Route path="/profile" element={<ProfilePage />} />
+                        <Route
+                            path="/description/:title"
+                            element={<DescPage />}
+                        />
+                    </>
+                )}
 
                 <Route path="*" element={<NotFound />} />
             </Routes>
