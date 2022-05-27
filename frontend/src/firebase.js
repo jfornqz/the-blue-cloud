11909d96a39835594b686d525b05627e37de7b53
pspapleng <chayanin3343@gmail.com>
2022-05-27 22:00:37 +0700

    chore: debug firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_APP_ID,
 }
-
+console.debug(firebaseConfig)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
