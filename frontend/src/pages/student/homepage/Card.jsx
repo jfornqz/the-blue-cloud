4b4935c18428473f64db5f731021f1d0f701d99c
chulalak <56313629+chayanpx@users.noreply.github.com>
2022-05-27 18:52:05 +0700

    update: add card to homepage

--- frontend/src/pages/student/homepage/Card.jsx
+++ frontend/src/pages/student/homepage/Card.jsx
@@ -1,14 +1,23 @@
 import { Fragment } from 'react'
-
-const Card = () => {
-
-    return (
-        <Fragment>
-            <div className='w-1/2 h-72 rounded-xl shadow-lg border border-gray-200'>
-
-            </div>
-        </Fragment>
-    )
+ 
+const Card = (title, post_by, desc, timestamp, images) => {
+ 
+   return (
+       <Fragment>
+           <div className='w-1/2 h-72 rounded-xl shadow-lg border border-gray-200'>
+               <div className='mt-3 mb-2 mx-4 inline-flex'>
+                   <p>creator fullname</p>
+                   <p className='ml-4'>timestamp</p>
+               </div>
+               <div className='text-md font-semibold ml-4'>
+                   <p>title</p>
+               </div>
+               <div className='text-md mt-3 ml-4'>
+                   <p>desc</p>
+               </div>
+           </div>
+       </Fragment>
+   )
 }
-
+ 
 export default Card
