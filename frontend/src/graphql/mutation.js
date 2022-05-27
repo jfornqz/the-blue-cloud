b9436ffd59962883e6a671cf8bf49c9cb4d4f3da
pspapleng <chayanin3343@gmail.com>
2022-05-27 18:25:53 +0700

    update: add mutation

--- frontend/src/graphql/mutation.js
+++ frontend/src/graphql/mutation.js
@@ -10,3 +10,26 @@ export const LOGIN = gql`
         }
     }
 `
+
+export const CREATE_SUBMISSION = gql`
+    mutation CreateOneSubmission($record: CreateOneSubmissionInput!) {
+        createOneSubmission(record: $record) {
+            record {
+                _id
+            }
+        }
+    }
+`
+
+export const UPDATE_SUBMISSION = gql`
+    mutation UpdateSubmissionId(
+        $id: MongoID!
+        $record: UpdateByIdSubmissionInput!
+    ) {
+        updateSubmissionId(_id: $id, record: $record) {
+            record {
+                _id
+            }
+        }
+    }
+`
