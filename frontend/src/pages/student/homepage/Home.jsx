7e1dcff1c7b1802406ecd223eb9b0a465987f6da
pspapleng <chayanin3343@gmail.com>
2022-05-27 19:15:45 +0700

    chore: before refactor (check point)

--- frontend/src/pages/student/homepage/Home.jsx
+++ frontend/src/pages/student/homepage/Home.jsx
@@ -14,13 +14,13 @@ const Home = () => {
     return (
         <Fragment>
             <div className="w-full h-full flex space-x-10 overflow-y-auto">
-                <div className="w-1/4 h-full pt-5 flex justify-center">
+                <div className="w-3/4 h-full pt-5 flex justify-center">
                     <div className="w-2/3 h-1/3 shadow-2xl rounded-xl"></div>
                 </div>
 
-                <div className="grow h-full flex flex-col pt-5 space-y-10">
+                <div className="grow  h-full flex flex-col pt-5 space-y-5 flex-auto ">
                     <div className="w-full h-10 max-h-10 flex justify-start">
-                        <div className="relative w-1/2 h-10">
+                        <div className="relative w-3/4 h-10">
                             <input
                                 type="text"
                                 className="h-full w-full focus:outline-none pl-3 rounded-xl border border-gray-400"
@@ -42,11 +42,18 @@ const Home = () => {
                         </div>
                     </div>
 
-                    <div className="h-full w-full flex flex-col justify-start space-y-4 mt-3">
-                        <Card />
-                        <Card />
-                        <Card />
-                    </div>
+                    {items.map((post, index) => {
+                        return (
+                            <div className="h-full w-full flex flex-col justify-start space-y-4 mt-3">
+                                <Card
+                                    title={post?.title}
+                                    desc={post?.desc}
+                                    post_by={post?.post_by}
+                                    images={post?.images}
+                                />
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </Fragment>
