aedd921c82032d620e4fe3be53f2d8766b46f51e
Thotsawat Jantapitak <61070065@kmitl.ac.th>
2022-05-28 00:58:45 +0700

    add edit and update submission

--- frontend/src/graphql/mutation.js
+++ frontend/src/graphql/mutation.js
@@ -41,3 +41,25 @@ export const CREATE_ONE_FORM = gql`
         }
     }
 `
+export const CREATE_SUBMISSION = gql`
+    mutation CreateOneSubmission($record: CreateOneSubmissionInput!) {
+        createOneSubmission(record: $record) {
+            record {
+                _id
+            }
+        }
+    }
+    `
+
+
+export const UPDATE_SUBMISSION = gql`
+    mutation UpdateSubmissionId(
+        $id: MongoID!
+        $record: UpdateByIdSubmissionInput!
+    ) {
+        updateSubmissionId(_id: $id, record: $record) {
+            record {
+                _id
+            }
+        }
+    }`
